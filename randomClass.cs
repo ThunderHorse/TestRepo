ebcee94c9755718b743cbb8a694318e289f575ff
Jason.Pease <jason.pease@availity.com>
2013-08-15 21:31:47 -0400

    Forgot to add

---
+++ randomClass.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace TestNamespace
+{
+	public class TestClass
+	{
+   		private string testString;
+
+   		public void SetString(string testString)
+   		{
+   			this.testString = testString;
+   		}
+
+   		public string GetString()
+   		{
+   			return this.testString;
+   	}
+}
